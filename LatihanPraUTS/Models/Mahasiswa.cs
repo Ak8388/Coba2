a198febf212b0efca2e6b9eb7c20590b71346f1c
andri <andri@LAPTOP-6FVFR528>
2023-02-07 00:42:16 +0700

    cons

---
+++ LatihanPraUTS/Models/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿namespace LatihanPraUTS.Models
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Prodi { get; set; }
+
+        public Mahasiswa(string nim, string nama, string prodi)
+        {
+            Nim = nim;
+            Nama = nama;
+            Prodi = prodi;
+        }
+    }
+}
